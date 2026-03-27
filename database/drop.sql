cb07c01943c848b625665c1ed707097983e69012
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-03-27 05:33:11 +0530

    chore(db) add drop script for schema reset

---
+++ database/drop.sql
@@ -0,0 +1,7 @@
+--DROPPING SCHEMA master and all the TABLES inside master sellers,locations
+
+DROP SCHEMA IF EXISTS master CASCADE;
+
+--DROPPING SCHEMA finance and all the TABLES inside finance purchases,payments
+
+DROP SCHEMA IF EXISTS finance CASCADE;
