fd1880a0f03f0d89b3dde4770bdd9c4fe0492794
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-26 16:18:18 +0530

    fix(config): mapped SQLALCHEMY_DATABASE_URI to standard DATABASE_URL

--- src/config.py
+++ src/config.py
@@ -7,6 +7,6 @@ class Config:
 
      SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
-     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
      SQLALCHEMY_TRACK_MODIFICATIONS = False
