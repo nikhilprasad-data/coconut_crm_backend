1986d21bf33d4f78c3a4a59a96b1a15910765fed
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-27 09:34:34 +0530

    fix(cors): allow only vercel and localhost origins

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,19 @@ from flask import Flask
 from .db import db
 from .config import Config
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 def create_app():
      app=Flask(__name__)
+
+     CORS(app, resources= {
+          r"/*" : {
+               "origins" : [
+                    "https://coconut-crm-frontend.vercel.app",
+                    "http://localhost:3000"
+               ]
+          }
+     }, supports_credentials= True)
 
      app.config.from_object(Config)
 
